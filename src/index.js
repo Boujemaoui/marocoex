6fe170db7ece7215ed0bcf9ce555557e7113c0f3
Said <45308987+Boujemaoui@users.noreply.github.com>
2025-01-06 13:02:18 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App"; // Asegúrate de que la ruta sea correcta
+
+// Se monta la aplicación en el contenedor 'root'
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
